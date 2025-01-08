873eb6b1125e29c6f0d45fc0784be53c109d9313
Dartagnan17450 <jmisery2@myges.fr>
2025-01-08 13:40:48 +0100

    visee

--- Assets/Scenes/julien/script/RececiveAction.cs
+++ Assets/Scenes/julien/script/RececiveAction.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RececiveAction : MonoBehaviour
+public class ReceiveAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Maximum de points de vie
+    public int maxHitPoint = 5;
+
+    //Points de vie actuels
+    public int hitPoint = 0;
+
+    private void Start()
     {
-        
+        //Au début : Points de vie actuels = Maximum de points de vie
+        hitPoint = maxHitPoint;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //Permet de recevoir des dommages
+    public void GetDamage(int damage)
     {
-        
+        //Applique les dommages aux points de vies actuels
+        hitPoint -= damage;
+
+        //Si les point de vie sont inférieurs à 1 = Supprime l'objet
+        if (hitPoint < 1)
+        {
+            Destroy(gameObject);
+        }
     }
 }
